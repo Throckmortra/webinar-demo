a0a1bef6348567416063ee7c890a79e524279a51
athrockmorton <athrockmorton@ipponusa.com>
2017-01-26 12:04:44 -0500

    updated pom for Tomcat

--- src/main/java/com/ippon/WebinarDemoApplication.java
+++ src/main/java/com/ippon/WebinarDemoApplication.java
@@ -2,9 +2,16 @@ package com.ippon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WebinarDemoApplication {
+public class WebinarDemoApplication extends SpringBootServletInitializer{
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(WebinarDemoApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebinarDemoApplication.class, args);
