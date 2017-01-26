213429009b678f2049fdd11a6d1019db79aea49c
athrockmorton <athrockmorton@ipponusa.com>
2017-01-26 09:52:43 -0500

    initial commit

---
+++ src/main/java/com/ippon/WebinarDemoApplication.java
@@ -0,0 +1,12 @@
+package com.ippon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebinarDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebinarDemoApplication.class, args);
+	}
+}
